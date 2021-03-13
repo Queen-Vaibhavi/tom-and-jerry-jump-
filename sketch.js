d27a631d54d69bfedfa5d6b66280226d0ec4b85d
Queen-Vaibhavi <77841320+Queen-Vaibhavi@users.noreply.github.com>
2021-03-13 14:24:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var tom,jerry;
+var tomImage,jerryImage;
+var tomimg,jerryimg;
+var bg,bgImage;
+function preload() {
+
+    //load the images here
+    tomImage.loadImage("cat1.png");
+    tomimg.loadAnimation("cat2.png","cat3.png");
+
+    jerryImage.loadImage("mouse1.png");
+    jerryimg.loadAnimation("mouse2.png","mouse3.png");
+    
+    bgImage.loadImage("garden.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+     
+    var bg = createSprite(1000,1000,1800,800);
+    bg.addImage(bgImage);
+    bg.scale = 5;
+
+    var tom = createSprite(500,800,20,20);
+    tom.addImage(tomImage);
+    tom.scale= 0.7;
+    
+    var jerry = createSprite(800,500,20,20);
+    jerry.addImage(jerryImage);
+    jerry.scale= 0.7;
+    
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+    if(keyCode === LEFT_ARROW){
+        tom.velocityX = -5;
+        tom.addAnimation("tomRunning",tomimg);
+        tom.changeAnimation("tomRunning");
+    }
+  
+}
